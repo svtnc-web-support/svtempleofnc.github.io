a66c89bd3279a77441900f6b3fba9c5d6a0104c5
Kris Vishwanathan <kris@icloudnow.com>
2021-02-20 21:30:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var tabulate = function (data,columns) {
-  var table = d3.select('body').append('table')
+  var table = d3.select('body').append('table').id("t01")
 	var thead = table.append('thead')
 	var tbody = table.append('tbody')
 
